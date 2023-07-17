2f8534b7ec1fdb8091d715c65affd1d9feefbef5
Juarez Junior <jjuarez_junior@hotmail.com>
2023-07-17 20:35:20 -0300

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,86 @@
 //create web server
-//const http = require('http');
-//const hostname = 'localhost';
-//const port = 3000;
-//const server = http.createServer((req, res) => {
-//    res.statusCode = 200;
-//res.setHeader('Content-Type', 'text/plain');
-//res.end('Hello World');
-//});
-//server.listen(port, hostname, () => {
-//    console.log(`Server running at http://${hostname}:${port}/`);
-//});
-//create web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const commentsRouter = require('./routes/comments');
-const app = express();
-const hostname = 'localhost';
-const port = 3000;
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/comments', commentsRouter);
-//error handler
-app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    res.status(status).send(err.message);
+var express = require('express');
+var router = express.Router();
+var commentModel = require('../models/comments.js');
+var postModel = require('../models/posts.js');
+var async = require('async');
+
+//get comments
+router.get('/', function(req, res) {
+
+    //get all comments from db
+    commentModel.find({}, function(err, comments) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.render('comments/index', { comments: comments });
+        }
+    });
 });
-//start server
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+//new comment
+router.get('/new', function(req, res) {
+    res.render('comments/new');
 });
+
+//create comment
+router.post('/', function(req, res) {
+    //create new comment
+    commentModel.create(req.body.comment, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            //find post
+            postModel.findById(req.body.comment.post, function(err, post) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    //add comment to post
+                    post.comments.push(comment);
+                    post.save(function(err, post) {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).send('Internal Server Error');
+                        } else {
+                            //redirect to comments
+                            res.redirect('/comments');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+//show comment
+router.get('/:id', function(req, res) {
+    //find comment by id
+    commentModel.findById(req.params.id).populate('post').exec(function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            //render comment
+            res.render('comments/show', { comment: comment });
+        }
+    });
+});
+
+//edit comment
+router.get('/:id/edit', function(req, res) {
+    //find comment by id
+    commentModel.findById(req.params.id, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            //render edit form
+            res.render('comments/edit', { comment: comment });
+        }
+    });
+});
+
+//
